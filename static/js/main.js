95037e2cb60ac2881102145baf6eecf968ab8590
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2025-03-16 17:49:58 +0000

    animation javascript in tips

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+const the_animation = document.querySelectorAll('.tip-container');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-tip-container');
+        } else {
+            entry.target.classList.remove('scroll-tip-container');
+        }
+    });
+}, { threshold: 0.5 });
+
+for (const element of the_animation) {
+    observer.observe(element);
+}
