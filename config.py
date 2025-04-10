4fe8b1e125d53c876cb4d8e35ec08f7bf74759f0
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2025-04-10 18:42:10 +0100

    resolving database render

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
-# DATABASE_URI = "postgresql://postgres:Toni2207@localhost/nihongo_db"
+DATABASE_URL = "postgresql://postgres:Toni2207@localhost/nihongo_db"
 
-DATABASE_URI = os.environ.get("DATABASE_URL")
+# SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@host:port/nihongo_quest'
+
+# DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "0000"
-    SQLALCHEMY_DATABASE_URI = DATABASE_URI
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
