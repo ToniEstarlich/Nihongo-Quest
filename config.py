67a596044413d74c3f7668dff72b69e974bbb644
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2025-04-17 19:07:25 +0100

    change the app for up Heroku

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 #DATABASE_URL = "postgresql://postgres:Toni2207@localhost/nihongo_db"
 # DATABASE_URL = os.environ.get("DATABASE_URL")
@@ -9,6 +9,8 @@ load_dotenv()
 class Config:
     # SECRET_KEY = os.environ.get("SECRET_KEY") or "0000"
     # SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    SECRET_KEY = os.environ.get("SECRET_KEY", "0000")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #SECRET_KEY = os.environ.get("SECRET_KEY", "0000")
+   # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = "0000"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Toni2207@localhost/nihongo_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
