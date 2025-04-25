82c4678bb72207d6efcd4180cc75d0ade6474899
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2025-04-25 13:56:51 +0100

    Update config DATABASE_URL for heroku

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
 import os
-#from dotenv import load_dotenv
-
-#load_dotenv()
-
-#DATABASE_URL = "postgresql://postgres:Toni2207@localhost/nihongo_db"
-# DATABASE_URL = os.environ.get("DATABASE_URL")
 
 class Config:
-    # SECRET_KEY = os.environ.get("SECRET_KEY") or "0000"
-    # SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    #SECRET_KEY = os.environ.get("SECRET_KEY", "0000")
-   # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SECRET_KEY = "0000"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "0000")
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Toni2207@localhost/nihongo_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
