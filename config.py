a84a8a33729a21fb08e2edc95cffd0e04bd092e2
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2025-05-20 17:37:53 +0100

    SQL connect

--- config.py
+++ config.py
@@ -11,5 +11,6 @@ class Config:
     uri = os.environ.get("DATABASE_URL")
     if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
+    
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
